e60d3c0e20a913c3a431b4107d22f1d122600138
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-08-02 19:05:39 +0100

    feat : add and setup Vuetify

---
+++ plugins/vuetify.js
@@ -0,0 +1,16 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#0F766E',
+          secondary: '#334155',
+        },
+      },
+    },
+  },
+})
