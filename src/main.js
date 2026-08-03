f809ae2d7e1b25d80f0adbe1326987b41d18e835
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-08-03 22:48:34 +0100

    feat : add interceptor to main flow to check auth o api

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from '../plugins/vuetify'
-
+import '@/api/interceptors.js' 
 const app = createApp(App)
 
 app.use(createPinia())
