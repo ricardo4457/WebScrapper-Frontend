39cdc3338610de05d6e1dd5c15942cab9218b9e4
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-08-03 13:12:37 +0100

    feat : update routes folder with the main routes of the project

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: () => import('@/views/DashboardView.vue'),
+  },
+  {
+    path: '/books',
+    name: 'books',
+    component: () => import('@/views/BooksView.vue'),
+  },
+  {
+    path: '/scraper',
+    name: 'scraper',
+    component: () => import('@/views/ScraperView.vue'),
+  },
+  {
+    path: '/schools',
+    name: 'schools',
+    component: () => import('@/views/SchoolsView.vue'),
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes,
 })
 
 export default router
