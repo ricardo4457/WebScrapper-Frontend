24fa2151eaef46042bb448e8c08f3310679f78bb
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-08-07 17:51:37 +0100

    feat : use computed step precist the current step name

--- src/components/search-flow/SearchWizard.vue
+++ src/components/search-flow/SearchWizard.vue
@@ -2,20 +2,26 @@
   <div class="search-wizard">
     <DistrictStep v-if="searchStore.currentStepName === 'district'" />
     <CityStep v-else-if="searchStore.currentStepName === 'city'" />
+    <YearStep v-if="step === 'year'" />
     <SchoolStep v-else-if="searchStore.currentStepName === 'school'" />
     <TeachingCycleStep v-else-if="searchStore.currentStepName === 'teachingCycle'" />
     <CourseStep v-else-if="searchStore.currentStepName === 'course'" />
     <DisciplineStep v-else-if="searchStore.currentStepName === 'discipline'" />
-    <ConfirmationStep v-else-if="searchStore.currentStepName === 'confirmation'" @searched="$emit('searched')" />
+    <ConfirmationStep
+      v-else-if="searchStore.currentStepName === 'confirmation'"
+      @searched="$emit('searched')"
+    />
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { useSearchStore } from '@/stores/search.store.js'
+import YearStep from './steps/YearStep.vue'
+import TeachingCycleStep from './steps/TeachingCycleStep.vue'
 import DistrictStep from './steps/DistrictStep.vue'
 import CityStep from './steps/CityStep.vue'
 import SchoolStep from './steps/SchoolStep.vue'
-import TeachingCycleStep from './steps/TeachingCycleStep.vue'
 import CourseStep from './steps/CourseStep.vue'
 import DisciplineStep from './steps/DisciplineStep.vue'
 import ConfirmationStep from './steps/ConfirmationStep.vue'
@@ -23,4 +29,6 @@ import ConfirmationStep from './steps/ConfirmationStep.vue'
 defineEmits(['searched'])
 
 const searchStore = useSearchStore()
+
+const step = computed(() => searchStore.currentStepName)
 </script>
