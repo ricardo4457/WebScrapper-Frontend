a1300209f9c310087c0fc0ad313646cd9c8cf893
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-08-07 18:56:22 +0100

    feat : add missing year step

---
+++ src/components/search-flow/steps/YearStep.vue
@@ -0,0 +1,25 @@
+<template>
+  <WizardStep title="Ano">
+    <v-select :items="schoolsStore.years" label="Ano" variant="outlined" v-model="selectedYear" />
+
+    <v-btn color="primary" class="mt-4" :disabled="!selectedYear" @click="onContinue">
+      Continuar
+    </v-btn>
+  </WizardStep>
+</template>
+<script setup>
+import { ref } from 'vue'
+import WizardStep from '../WizardStep.vue'
+import { useSchoolsStore } from '@/stores/schools.store.js'
+import { useSearchStore } from '@/stores/search.store.js'
+
+const schoolsStore = useSchoolsStore()
+const searchStore = useSearchStore()
+
+const selectedYear = ref(searchStore.selections.year)
+
+function onContinue() {
+  searchStore.setSelection('year', selectedYear.value)
+  searchStore.nextStep()
+}
+</script>
