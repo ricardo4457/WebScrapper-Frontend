16b5ecf98f6074752a30bec387363126c725022d
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-08-07 12:04:56 +0100

    feat : banner for waiting results

---
+++ src/components/common/AsyncStatusBanner.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-alert type="info" variant="tonal" class="mb-4" border="start">
+    <div class="d-flex align-center">
+      <v-progress-circular indeterminate size="20" width="2" class="mr-3" />
+      <span>{{ message }}</span>
+    </div>
+  </v-alert>
+</template>
+
+<script setup>
+defineProps({
+  message: { type: String, default: 'A recolher informação...' },
+})
+</script>
