a56bf4c629d580ee81a6982f28b67f441550761c
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-08-10 21:59:28 +0100

    feat : add empty state to course step

--- src/components/search-flow/steps/CourseStep.vue
+++ src/components/search-flow/steps/CourseStep.vue
@@ -12,6 +12,12 @@
       class="d-flex mx-auto my-6"
     />
 
+    <EmptyState
+      v-else-if="!schoolsStore.courses.length"
+      title="Sem cursos conhecidos"
+      subtitle="Podes avançar sem escolher curso."
+    />
+
     <v-list v-else lines="one">
       <v-list-item
         v-for="course in schoolsStore.courses"
@@ -20,6 +26,10 @@
         @click="onSelect(course)"
       />
     </v-list>
+
+    <v-btn v-if="!schoolsStore.coursesLoading" variant="text" class="mt-4" @click="onSkip">
+      Saltar este passo
+    </v-btn>
   </WizardStep>
 </template>
 
@@ -27,18 +37,19 @@
 import { onMounted } from 'vue'
 import WizardStep from '../WizardStep.vue'
 import ErrorState from '@/components/common/ErrorState.vue'
+import EmptyState from '@/components/common/EmptyState.vue'
 import { useSchoolsStore } from '@/stores/schools.store.js'
 import { useSearchStore } from '@/stores/search.store.js'
 
 const schoolsStore = useSchoolsStore()
 const searchStore = useSearchStore()
 
-async function load() {
+function load() {
   const school = searchStore.selections.school
 
   if (!school?.id) return
 
-  await schoolsStore.fetchCourses(school.id, {
+  return schoolsStore.fetchCourses(school.id, {
     year: searchStore.selections.year,
     teachingCycle: searchStore.selections.teachingCycle,
   })
@@ -50,6 +61,11 @@ onMounted(() => {
 
 function onSelect(course) {
   searchStore.setSelection('course', course)
+  searchStore.nextStep()
+}
+
+function onSkip() {
+  searchStore.setSelection('course', null)
   searchStore.nextStep()
 }
 </script>
